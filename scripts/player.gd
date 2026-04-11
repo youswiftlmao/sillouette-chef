cc95d1a439f9d3c0156eb6e2e3ed512c961af1c1
yousef <ykhanbugatti@gmail.com>
2026-04-11 16:41:12 +0400

    added sfx for the players jump walk and landing working on level design atm and inventory/checklist component will be worked on soon aswlas moving platfroms adninteractable drop to pick up items from the todo list

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,11 +6,19 @@ const JUMP_VELOCITY = -270.0
  
 @onready var chef: AnimatedSprite2D = $chef
 
+var was_on_floor: bool = false
+
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		# sfx for ump landing cuz idk where 2 put it
+	if is_on_floor() and not was_on_floor:
+		$land_thud.play()
+
+	was_on_floor = is_on_floor()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -30,16 +38,31 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		if direction == 0:
 			chef.play("idle")
+			$Footstep.stop()
+			$FootstepTimer.stop()
 		else:
 			chef.play("run")
+
+			if $FootstepTimer.is_stopped():
+				$Footstep.play()      # instant first step
+				$FootstepTimer.start()
+
+	else:
+		$Footstep.stop()
+		$FootstepTimer.stop()
+
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		$jump.play()
 		chef.play("jump")
-
-	
+		$Footstep.stop()
+		$FootstepTimer.stop()
+			
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_time_footstep_timer_timeout() -> void:
+	$Footstep.play()
