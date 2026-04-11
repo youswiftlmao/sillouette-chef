8747b5908ceaa2ee7611063c1f5a5106048f311a
yousef <ykhanbugatti@gmail.com>
2026-04-11 15:15:44 +0400

    added and implemented  all animaitons started on lvl design and player sfx

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 175.0
+const JUMP_VELOCITY = -270.0
+ 
+@onready var chef: AnimatedSprite2D = $chef
 
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +13,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
+	
+	#flips sprite 
+	if direction > 0:
+		chef.flip_h = false
+	elif direction < 0 :
+		chef.flip_h = true
+		 
+	#animations
+	if is_on_floor():
+		if direction == 0:
+			chef.play("idle")
+		else:
+			chef.play("run")
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$jump.play()
+		chef.play("jump")
+
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
