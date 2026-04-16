c5e8790d734c85c21c28cbafe4510829a91e97e7
yousef <ykhanbugatti@gmail.com>
2026-04-16 18:35:23 +0400

    finished level design for lvl 2 created and makde working  the pickups fixed a few platform issues made the portal work cleaned up the coding system slightly will now work on decor and level 3 woo

--- scripts/portal2.gd
+++ scripts/portal2.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var cantp = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+		if body.has_method("player") and cantp:
+			Transition.fade_to_scene("res://scenes/game.tscn")
+
+
+func _on_thechef_cango_2_nextlvl() -> void:
+	cantp = true
